50454e3dcd41be65004b785885884a1ce60ac801
Luango <luangreturn@gmail.com>
2017-06-09 15:01:48 -0400

    Arrow speed

--- Assets/Scripts/Camera/CharacterCamera.cs
+++ Assets/Scripts/Camera/CharacterCamera.cs
@@ -12,7 +12,7 @@ public class CharacterCamera : MonoBehaviour {
         Mouse_Position = Input.mousePosition;
         Mouse_Position.z = 0f;
         mouse_world_position = Camera.main.ScreenToWorldPoint(Mouse_Position);
-        mouse_world_position = new Vector3(Mathf.Clamp(mouse_world_position.x - player.position.x, -25f, 25f) / 2.5f, Mathf.Clamp(mouse_world_position.y - player.position.y, -1f, 15f) / 3.5f, 0f);
+        mouse_world_position = new Vector3(Mathf.Clamp(mouse_world_position.x - player.position.x, -25f, 25f) / 2.5f, Mathf.Clamp(mouse_world_position.y - player.position.y, -15f, 15f) / 2.5f, 0f);
 
         transform.position = new Vector3(player.position.x + mouse_world_position.x, player.position.y + mouse_world_position.y + 3f, -10f); // Camera follows the player with specified offset positio
         //transform.position = new Vector3(player.position.x, player.position.y + 3f, -10f); // Camera follows the player with specified offset position
