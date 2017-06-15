b586bce7d40809a79f374e632066609afa63b9f2
Luango <luangreturn@gmail.com>
2017-06-14 23:35:49 -0400

    Add Transport func

--- Assets/Scripts/Items/Arrows/Space_Arrow.cs
+++ Assets/Scripts/Items/Arrows/Space_Arrow.cs
@@ -3,10 +3,13 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Space_Arrow : Normal_Arrow {
-    private void Start()
+    public GameObject Yijo;
+
+    void Start()
     {
         LifeSpan = 15f;
         arrow_damage = 0f;
+        Yijo = GameObject.FindGameObjectWithTag("Player");
     }
     void Update()
     {
@@ -20,5 +23,17 @@ public class Space_Arrow : Normal_Arrow {
         {
             LifeSpan -= Time.deltaTime;
         }
+        if (Input.GetKeyDown("space"))
+        {
+            TransportYijo();
+        }
     }
+
+    // Transport Yijo
+    private void TransportYijo()
+    {
+        Yijo.transform.position = transform.position;
+    }
+
+    // Following Camera
 }
