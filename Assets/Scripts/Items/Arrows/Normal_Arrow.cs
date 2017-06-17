8683c5e9dc0ac923b0020e81a7159c3ff7372c07
Luango <luangreturn@gmail.com>
2017-06-17 00:01:48 -0400

    Trigger Arrow and Collision Arrow

--- Assets/Scripts/Items/Arrows/Normal_Arrow.cs
+++ Assets/Scripts/Items/Arrows/Normal_Arrow.cs
@@ -12,9 +12,19 @@ public class Normal_Arrow : MonoBehaviour {
     protected float arrow_damage = 33f;
     public void OnTriggerEnter2D(Collider2D collision)
     {
-        gameObject.transform.parent = collision.gameObject.transform;
-        Debug.Log("Ground");
-        Destroy(this);
+        string _tag = collision.gameObject.tag;
+        if (_tag == "Ground")
+        {
+            gameObject.transform.parent = collision.gameObject.transform;
+            BoxCollider2D[] BoxCollider2Ds;
+            BoxCollider2Ds = gameObject.GetComponents<BoxCollider2D>();
+            foreach (BoxCollider2D collider in BoxCollider2Ds)
+            {
+                collider.enabled = false;
+            }
+            Debug.Log("Ground");
+            Destroy(this);
+        }
     }
     public void OnCollisionEnter2D(Collision2D collision)
     {
@@ -24,10 +34,13 @@ public class Normal_Arrow : MonoBehaviour {
             if(_tag == "Mage") {
                 collision.gameObject.GetComponent<Mage>().getDamaged(arrow_damage);
                 gameObject.transform.parent = collision.gameObject.transform;
-                Destroy(this);
-            } else if (_tag == "Ground")
-            {
-                gameObject.transform.parent = collision.gameObject.transform;
+                BoxCollider2D[] BoxCollider2Ds;
+                BoxCollider2Ds = gameObject.GetComponents<BoxCollider2D>();
+                foreach (BoxCollider2D collider in BoxCollider2Ds)
+                {
+                    collider.enabled = false;
+                }
+                Debug.Log("Ground");
                 Destroy(this);
             }
         }
