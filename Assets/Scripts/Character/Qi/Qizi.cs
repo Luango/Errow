d2263f4978a3f9c7374880e713f720f0a4cff04f
Luango <luangreturn@gmail.com>
2017-08-22 23:55:06 -0400

    StartQizi

---
+++ Assets/Scripts/Character/Qi/Qizi.cs
@@ -0,0 +1,103 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Qizi : MonoBehaviour {
+    public bool ColorState; // white: true, black: false
+    private List<bool> AdjacentStatus = new List<bool>();
+    private List<RaycastHit2D> BoundaryRays;
+    private float RayDistance = 1.1f;
+    public enum Direction{
+        up,
+        down,
+        left,
+        right
+    }
+
+	// Use this for initialization
+	void Start () {
+        for (int i = 0; i < 4; i++)
+        {
+            AdjacentStatus.Add(false);
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		for(int i =0; i < 4; i++)
+        {
+            if(AdjacentStatus[i] == false)
+            {
+                break;
+   
+            }
+            if (AdjacentStatus[3] == true)
+            {
+                Destroy(this.gameObject);
+            }
+        }
+	}
+
+    private void FixedUpdate()
+    {
+        for (int i=0; i<4; i++)
+        {
+            Direction Curr_Dir = (Direction)i;
+            RaycastHit2D[] hits;
+            switch (Curr_Dir)
+            {
+                case Direction.up:
+                    hits = Physics2D.RaycastAll(new Vector2(transform.position.x, transform.position.y), Vector2.up, RayDistance); 
+                    foreach(RaycastHit2D hit in hits)
+                    {
+                        if(hit.collider.gameObject != this.gameObject)
+                        {
+                            print(this.gameObject + " Up has " + hit.collider.gameObject);
+                            AdjacentStatus[(int)Direction.up] = true;
+                            break;
+                        }
+                    }
+                    AdjacentStatus[(int)Direction.up] = false;
+                    break;
+                case Direction.down:
+                    hits = Physics2D.RaycastAll(new Vector2(transform.position.x, transform.position.y), -Vector2.up, RayDistance);
+                    foreach (RaycastHit2D hit in hits)
+                    {
+                        if (hit.collider.gameObject != this.gameObject)
+                        {
+                            print(this.gameObject + " Down has " + hit.collider.gameObject);
+                            AdjacentStatus[(int)Direction.down] = true;
+                        }
+                    } 
+                    AdjacentStatus[(int)Direction.down] = false;
+                    break;
+                case Direction.left:
+                    hits = Physics2D.RaycastAll(new Vector2(transform.position.x, transform.position.y), Vector2.left, RayDistance);
+                    Debug.DrawRay(transform.position, Vector2.left, Color.green, 2, false);
+                    foreach (RaycastHit2D hit in hits)
+                    {
+                        if (hit.collider.gameObject != this.gameObject)
+                        {
+                            print(this.gameObject + " Left has " + hit.collider.gameObject);
+                            AdjacentStatus[(int)Direction.left] = true;
+                        }  
+                    }
+                    AdjacentStatus[(int)Direction.left] = false;
+                    break;
+                case Direction.right:
+                    hits = Physics2D.RaycastAll(transform.position, -Vector2.left, RayDistance);
+                    Debug.DrawRay(transform.position, -Vector2.left, Color.green, 2, false);
+                    foreach (RaycastHit2D hit in hits)
+                    {
+                        if (hit.collider.gameObject != this.gameObject)
+                        {
+                            print(this.gameObject + " Right has " + hit.collider.gameObject);
+                            AdjacentStatus[(int)Direction.right] = true;
+                        } 
+                    }
+                    AdjacentStatus[(int)Direction.right] = false;
+                    break;
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/Items/EducationShooter/EducationBullet.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EducationBullet : MonoBehaviour {
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        print("hi collision");
+        if (collision.tag == "Player" || collision.tag == "Others")
+        {
+            print("hi player");
+            Destroy(this.gameObject);
+        }
+    }
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
