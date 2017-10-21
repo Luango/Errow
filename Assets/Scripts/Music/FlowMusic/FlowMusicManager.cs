63f0125b4282a9edcf01e11322abc48d3eda5d16
Luango <luangreturn@gmail.com>
2017-10-21 17:32:59 -0400

    Adding flow music mechanism :dart:

---
+++ Assets/Scripts/Music/FlowMusic/FlowMusicManager.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlowMusicManager : MonoBehaviour { 
+    public TextAsset sheetMusic;
+    public float deltaTime;
+
+    private static FlowMusicManager instance = null;
+    public static FlowMusicManager Instance
+    {
+        get
+        {
+            return instance;
+        }
+    }
+     
+    private void Awake()
+    {
+        if (instance != null && instance != this)
+        {
+            Destroy(this.gameObject);
+        }
+
+        instance = this;
+        DontDestroyOnLoad(this.gameObject);
+    }
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void NextLine()
+    {
+
+    }
+
+    void ReadSheetCreateNotes()
+    {
+        string[] linesInFile = sheetMusic.text.Split('\n');
+        int lineNo = 0;
+        foreach (string line in linesInFile)
+        {
+            string[] notesInLine = line.Split(new char[0]);
+
+            foreach (string note in notesInLine)
+            { 
+
+            }
+            lineNo++;
+        }
+    }
+}
---
+++ Assets/Scripts/Music/FlowMusic/FlowMusicNote.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlowMusicNote : MonoBehaviour {
+    private Vector3 iniPosition;
+
+    private float lifeSpan;
+    private string noteName;
+    private float stepSize;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
