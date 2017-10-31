180363485a5e2049d5a25a7afefb18f6490f8136
Luango <luangreturn@gmail.com>
2017-10-30 22:59:27 -0400

    Fluid the music flow

--- Assets/Scripts/Music/FlowMusic/FlowMusicManager.cs
+++ Assets/Scripts/Music/FlowMusic/FlowMusicManager.cs
@@ -10,8 +10,9 @@ public class FlowMusicManager : MonoBehaviour {
     private int lineNo = 0;
     private Vector3 prePos;
     private int stepsCount;
-    private float stepSize = 0.048f;
+    private float stepSize = 0.2f;
     private static FlowMusicManager instance = null;
+    private Vector2 FlowDirection;
 
     public static FlowMusicManager Instance
     {
@@ -45,6 +46,8 @@ public class FlowMusicManager : MonoBehaviour {
     void Start () {
         prePos = FlowMusicPlayer.Instance.transform.position + new Vector3(Random.Range(-15f, 15f), Random.Range(-15f, 15f), 0f);
         stepsCount = 0;
+        FlowDirection = new Vector2(Random.Range(-1f, 1f), Random.Range(-1f, 1f));
+        FlowDirection.Normalize();
     }
 
     private void FixedUpdate()
@@ -67,15 +70,27 @@ public class FlowMusicManager : MonoBehaviour {
                         stepsCount++;
                     }
                     if (musicNote != null)
-                    {
-                        print(musicNote);
-                        Vector3 newPos = prePos + new Vector3(Random.Range(-8f, 8f)*stepSize*stepsCount, Random.Range(-8f, 8f) * stepSize * stepsCount, 0f);
+                    { 
+                        Vector3 newPos = prePos + new Vector3(FlowDirection.x * stepSize * stepsCount* Random.Range(1f,2.5f), FlowDirection.y * stepSize * stepsCount * Random.Range(1f, 1.5f), 0f);
                         GameObject noteObj = (GameObject)Instantiate(musicNote, newPos, Quaternion.identity);
                         noteObj.GetComponent<AudioSource>().enabled = true;
                         noteObj.GetComponent<SpriteRenderer>().enabled = true;
                         noteObj.GetComponent<FlowMusicNote>().enabled = true;
                         prePos = newPos;
                         stepsCount = 0;
+
+                        var turnPossible = Random.Range(0f, 1f);
+                        if (turnPossible > 0.05f)
+                        {
+                            FlowDirection = new Vector2(FlowDirection.x + Random.Range(-0.6f, 0.6f), FlowDirection.y + Random.Range(-0.6f, 0.6f));
+                            FlowDirection.Normalize();
+                        }
+                        else
+                        {
+
+                            FlowDirection = new Vector2(Random.Range(-1f, 1f), Random.Range(-1f, 1f));
+                            FlowDirection.Normalize();
+                        }
                     }
                 }
             }
