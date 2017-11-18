576a9e3666a53fb46379648fd7e35f1039c87c99
Luango <luangreturn@gmail.com>
2017-11-17 23:44:33 -0500

    Boat trail.

--- Assets/Scripts/Music/FlowMusic/FlowMusicPlayer.cs
+++ Assets/Scripts/Music/FlowMusic/FlowMusicPlayer.cs
@@ -31,30 +31,29 @@ public class FlowMusicPlayer : MonoBehaviour {
     void Start () { 
         //transform.DOScale(9.5f, 0.804f).SetLoops(-1, LoopType.Yoyo);
         body = GetComponent<Rigidbody2D>();
-    }
-	
-	// Update is called once per frame
-	void FixedUpdate () {
+    } 
+
+    // Update is called once per frame
+    void FixedUpdate () {
 
 #if UNITY_STANDALONE || UNITY_WEBPLAYER || UNITY_EDITOR
         if (Input.GetAxisRaw("Horizontal") != 0 || Input.GetAxisRaw("Vertical") != 0)
         {
             Vector2 v = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
-
-            float angle = Mathf.Atan2(v.y, v.x) * Mathf.Rad2Deg;
+            v.Normalize();
+            float angle = Mathf.Atan2(v.y, v.x) * Mathf.Rad2Deg - 90f;
             Quaternion rotation = Quaternion.AngleAxis(angle, Vector3.forward);
             transform.rotation = Quaternion.Slerp(transform.rotation, rotation, Time.deltaTime*2f);
             body.AddForce(v * speed, ForceMode2D.Force);
         }
-          
+
 #elif UNITY_IOS || UNITY_ANDROID || UNITY_WP8 || UNITY_IPHONE
         // use accleration control 
-        Vector2 v2 = new Vector2(Input.acceleration.x, Input.acceleration.y); 
-        body.AddForce(v2 * speed * 5f, ForceMode2D.Force);
-
-        float angle = Mathf.Atan2(v2.y, v2.x) * Mathf.Rad2Deg;
+        Vector2 v2 = new Vector2(Input.acceleration.x, Input.acceleration.y);  
+        float angle = Mathf.Atan2(v2.y, v2.x) * Mathf.Rad2Deg - 90f;
         Quaternion rotation = Quaternion.AngleAxis(angle, Vector3.forward);
         transform.rotation = Quaternion.Slerp(transform.rotation, rotation, Time.deltaTime * 2f);
+        body.AddForce(v2 * speed * 5f, ForceMode2D.Force);
 #endif
     }
 }
