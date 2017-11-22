bae6dc01cf3dd2bb9941e008fec560a2afd9c4d4
Luango <luangreturn@gmail.com>
2017-11-21 22:14:31 -0500

    Add story manager.

---
+++ Assets/Scripts/Music/FlowMusic/StoryManager.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StoryManager : MonoBehaviour {
+    private List<GameObject> StoryNotes;
+
+	// Use this for initialization
+	void Start () {
+		foreach(Transform child in transform)
+        {
+            StoryNotes.Add(child.gameObject);
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
